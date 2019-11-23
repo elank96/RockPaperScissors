b7198a812089fd987b3a49b854411dd57930fca5
Elan Kainen <elank96@gmail.com>
2019-11-22 20:51:30 -0500

    application completed

--- RockPaperScissors/ResultsViewController.swift
+++ RockPaperScissors/ResultsViewController.swift
@@ -11,22 +11,19 @@ import UIKit
 class ResultsViewController: UIViewController {
 
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var winnerImage: UIImageView!
+    
+    var resultTxt: String = ""
+    var imageName: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        resultLabel.text = resultTxt
+        self.winnerImage.image = UIImage(named: imageName!)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    @IBAction func playAgain(_ sender: Any) {
+           dismiss(animated: true, completion: nil)
+       }
 
 }
--- RockPaperScissors/SelectionViewController.swift
+++ RockPaperScissors/SelectionViewController.swift
@@ -16,7 +16,7 @@ class SelectionViewController: UIViewController {
 
     func randomMoveGenerator() -> String {
         
-        let moves = ["Rock", "Paper", "Scissor"]
+        let moves = ["Rock", "Paper", "Scissors"]
         
         let random = moves[Int(arc4random_uniform(UInt32(moves.count)))]
     
@@ -24,14 +24,75 @@ class SelectionViewController: UIViewController {
     }
     
     @IBAction func rock(_ sender: Any) {
+
         let computerMove = randomMoveGenerator()
-        //use switch case here
+        let resultsVC: ResultsViewController
+               
+       resultsVC = storyboard?.instantiateViewController(identifier: "ResultsViewController") as! ResultsViewController
+        
+        switch computerMove {
+        case "Rock":
+            resultsVC.resultTxt = "Rock on Rock...Thats a tie!"
+            resultsVC.imageName = "Tie"
+        case "Paper":
+            resultsVC.resultTxt = "Paper covers rock. You lose!"
+            resultsVC.imageName = "PaperCoversRock"
+        case "Scissors":
+            resultsVC.resultTxt = "Rock beats Scissors. You win!"
+            resultsVC.imageName = "RockCrushesScissors"
+        default:
+            print("Something alternative")
+        }
+        
+        present(resultsVC, animated: true, completion: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let resultsVC = segue.destination as! ResultsViewController
+        
+        let computerMove = randomMoveGenerator()
+                   
+        if(segue.identifier == "paperSegue"){
+            switch computerMove {
+            case "Rock":
+                resultsVC.resultTxt = "Paper covers rock. You win!"
+                resultsVC.imageName = "PaperCoversRock"
+            case "Paper":
+                resultsVC.resultTxt = "Paper on Paper...Thats a tie!"
+                resultsVC.imageName = "Tie"
+            case "Scissors":
+                resultsVC.resultTxt = "Scissors beats paper. You lose!"
+                resultsVC.imageName = "ScissorsCutPaper"
+            default:
+                print("Something alternative")
+            }
+        }
+        else {
+            switch computerMove {
+            case "Rock":
+                resultsVC.resultTxt = "Rock beats Scissors. You lose!"
+                resultsVC.imageName = "RockCrushesScissors"
+            case "Paper":
+                resultsVC.resultTxt = "Scissors beats paper. You win!"
+                resultsVC.imageName = "ScissorsCutPaper"
+            case "Scissors":
+                resultsVC.resultTxt = "Scissors on Scissors...Thats a tie!"
+                resultsVC.imageName = "Tie"
+            default:
+                print("Something alternative")
+            }
+        }
     }
     
     @IBAction func paper(_ sender: Any) {
+        performSegue(withIdentifier: "paperSegue", sender: self)
     }
     
-    @IBAction func scissor(_ sender: Any) {
+    @IBAction func scissors(_ sender: Any) {
+        performSegue(withIdentifier: "scissorsSegue", sender: self)
     }
+    
+    
 }
 
